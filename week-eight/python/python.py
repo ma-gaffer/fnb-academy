953271632e60d2d9e8d86445d1b500d59633f601
Mark  Flavian Dube <markflaviandube12@gmail.com>
2025-06-28 21:19:46 +0200

    Add files via upload

---
+++ week-eight/python/python.py
@@ -0,0 +1,23 @@
+#operators
+
+#addition(+)
+#subtraction(-)
+#multiplication(*)
+#division(/)
+#modulus(%)
+#exponent(**)
+
+x = 10 
+y = 2 
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+x = 10 
+x -= 2 
+
+print(x)
